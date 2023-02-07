042d1ddad08a7e8df83fd2fd1e47d652f2f3ce4c
Taras.Kravets <taraskravets95@gmail.com>
2023-02-08 00:52:37 +0200

    feat(deploy): added deploy setup

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,5 +1,12 @@
 import io from 'socket.io-client';
 
-const socket = io(`${process.env.REACT_APP_API_URL}`);
+const socket = io(
+	`${process.env.REACT_APP_API_URL_DEPLOY || process.env.REACT_APP_API_URL}`,
+	{
+		query: {
+			headers: { 'User-Agent': 'Test task' },
+		},
+	}
+);
 
 export default socket;
